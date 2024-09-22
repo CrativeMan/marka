3d10063bc9dd4879cdbc06326c73f89d012ad4b4
crative <hannig.sebi@gmail.com>
2024-09-22 17:09:34 +0200

    HOME_KEY, END_KEY

--- main.c
+++ main.c
@@ -16,6 +16,8 @@ enum editorKey {
   ARROW_RIGHT,
   ARROW_UP,
   ARROW_DOWN,
+  HOME_KEY,
+  END_KEY,
   PAGE_UP,
   PAGE_DOWN
 };
@@ -89,13 +91,21 @@ int editorReadKey() {
           return '\x1b';
         if (seq[2] == '~') {
           switch (seq[1]) {
+          case '1':
+            return HOME_KEY;
+          case '4':
+            return END_KEY;
           case '5':
             return PAGE_UP;
           case '6':
             return PAGE_DOWN;
+          case '7':
+            return HOME_KEY;
+          case '8':
+            return END_KEY;
           }
         }
-      } else { // if is arrow sequence
+      } else {
         switch (seq[1]) {
         case 'A': // up arrow
           return ARROW_UP;
@@ -105,7 +115,18 @@ int editorReadKey() {
           return ARROW_RIGHT;
         case 'D': // left arrow
           return ARROW_LEFT;
+        case 'H':
+          return HOME_KEY;
+        case 'F':
+          return END_KEY;
         }
+      }
+    } else if (seq[0] == 'O') {
+      switch (seq[1]) {
+      case 'H':
+        return HOME_KEY;
+      case 'F':
+        return END_KEY;
       }
     }
     return '\x1b';
@@ -258,6 +279,14 @@ void editorProcessKeypress() {
     write(STDOUT_FILENO, "\x1b[2J", 4);
     write(STDOUT_FILENO, "\x1b[H", 3);
     exit(0);
+    break;
+
+  case HOME_KEY:
+    E.cx = 0;
+    break;
+
+  case END_KEY:
+    E.cx = E.screencols - 1;
     break;
 
   case PAGE_UP:
