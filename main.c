9a05031177c091712d3dc7fcb85bd1f0c1f14666
crative <hannig.sebi@gmail.com>
2024-09-22 17:04:44 +0200

    page up and page down

--- main.c
+++ main.c
@@ -11,7 +11,14 @@
 #define MARKA_VERSION "0.1"
 #define CTRL_KEY(k) ((k) & 0x1f)
 
-enum editorKey { ARROW_LEFT = 1000, ARROW_RIGHT, ARROW_UP, ARROW_DOWN };
+enum editorKey {
+  ARROW_LEFT = 1000,
+  ARROW_RIGHT,
+  ARROW_UP,
+  ARROW_DOWN,
+  PAGE_UP,
+  PAGE_DOWN
+};
 
 /* data */
 struct editorConfig {
@@ -76,16 +83,29 @@ int editorReadKey() {
     if (read(STDIN_FILENO, &seq[1], 1) != 1)
       return '\x1b';
 
-    if (seq[0] == '[') { // if starts with [
-      switch (seq[1]) {
-      case 'A': // up arrow
-        return ARROW_UP;
-      case 'B': // down arrow
-        return ARROW_DOWN;
-      case 'C': // right arrow
-        return ARROW_RIGHT;
-      case 'D': // left arrow
-        return ARROW_LEFT;
+    if (seq[0] == '[') {                    // if starts with [
+      if (seq[1] >= '0' && seq[1] <= '9') { // read in other bytes TODO comment
+        if (read(STDIN_FILENO, &seq[2], 1) != 1)
+          return '\x1b';
+        if (seq[2] == '~') {
+          switch (seq[1]) {
+          case '5':
+            return PAGE_UP;
+          case '6':
+            return PAGE_DOWN;
+          }
+        }
+      } else { // if is arrow sequence
+        switch (seq[1]) {
+        case 'A': // up arrow
+          return ARROW_UP;
+        case 'B': // down arrow
+          return ARROW_DOWN;
+        case 'C': // right arrow
+          return ARROW_RIGHT;
+        case 'D': // left arrow
+          return ARROW_LEFT;
+        }
       }
     }
     return '\x1b';
@@ -239,6 +259,14 @@ void editorProcessKeypress() {
     write(STDOUT_FILENO, "\x1b[H", 3);
     exit(0);
     break;
+
+  case PAGE_UP:
+  case PAGE_DOWN: { // move c up or down as many tms as needed
+    int times = E.screenrows;
+    while (times--) {
+      editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
+    }
+  } break;
 
   case ARROW_UP:
   case ARROW_DOWN:
