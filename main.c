d4d530e4cb3d60117e1fc2aa4852efa95e4b5c86
crative <hannig.sebi@gmail.com>
2024-09-22 20:28:43 +0200

    basic file drawinbasic file drawingg

--- main.c
+++ main.c
@@ -1,9 +1,14 @@
 /* includes */
+#define _DEFAULT_SOURCE
+#define _BSD_SOURCE
+#define _GNU_SOURCE
+
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/ioctl.h>
+#include <sys/types.h>
 #include <termios.h>
 #include <unistd.h>
 
@@ -24,10 +29,18 @@ enum editorKey {
 };
 
 /* data */
+typedef struct erow {
+  int size;
+  char *chars;
+} erow;
+
 struct editorConfig {
   int cx, cy;
+  int rowoff;
   int screenrows;
   int screencols;
+  int numrows;
+  erow *row;
   struct termios orig_termios;
 };
 
@@ -181,6 +194,40 @@ int getWindowSize(int *rows, int *cols) {
   }
 }
 
+/* row operations */
+void editorAppendRow(char *s, size_t len) {
+  E.row = realloc(E.row, sizeof(erow) * (E.numrows + 1));
+
+  int at = E.numrows;
+  E.row[at].size = len;
+  E.row[at].chars = malloc(len + 1);
+  memcpy(E.row[at].chars, s, len);
+  E.row[at].chars[len] = '\0';
+  E.numrows++;
+}
+
+/* file i/o */
+
+void editorOpen(char *filename) {
+  FILE *fp = fopen(filename, "r");
+  if (!fp)
+    die("fopen");
+
+  char *line = NULL;
+  size_t linecap = 0;
+  ssize_t linelen;
+  linelen = getline(&line, &linecap, fp);
+  while ((linelen = getline(&line, &linecap, fp)) != -1) {
+    while (linelen > 0 &&
+           (line[linelen - 1] == '\n' || line[linelen - 1] == '\r'))
+      linelen--;
+    editorAppendRow(line, linelen);
+  }
+
+  free(line);
+  fclose(fp);
+}
+
 /* append buffer */
 
 struct abuf {
@@ -204,25 +251,35 @@ void abAppend(struct abuf *ab, const char *s, int len) {
 void abFree(struct abuf *ab) { free(ab->b); }
 
 /* output */
+
 void editorDrawRows(struct abuf *ab) {
   int y;
   for (y = 0; y < E.screenrows; y++) { // for every row
-    if (y == E.screenrows / 3) {       // if row is third down monitor
-      char welcome[80];
-      int welcomelen = snprintf(welcome, sizeof(welcome),
-                                "Marka editor -- version %s", MARKA_VERSION);
-      if (welcomelen > E.screencols)
-        welcomelen = E.screencols;
-      int padding = (E.screencols - welcomelen) / 2;
-      if (padding) {
+    int filerow = y + E.rowoff;        // get the y in the file
+    if (filerow >= E.numrows) {        // check if text is part of row buffer
+      if (E.numrows == 0 &&
+          y == E.screenrows / 3) { // if row is third down monitor
+        char welcome[80];
+        int welcomelen = snprintf(welcome, sizeof(welcome),
+                                  "Marka editor -- version %s", MARKA_VERSION);
+        if (welcomelen > E.screencols)
+          welcomelen = E.screencols;
+        int padding = (E.screencols - welcomelen) / 2;
+        if (padding) {
+          abAppend(ab, "~", 1);
+          padding--;
+        }
+        while (padding--)
+          abAppend(ab, " ", 1);
+        abAppend(ab, welcome, welcomelen);
+      } else { // if not welcome message row
         abAppend(ab, "~", 1);
-        padding--;
       }
-      while (padding--)
-        abAppend(ab, " ", 1);
-      abAppend(ab, welcome, welcomelen);
-    } else { // if not welcome message row
-      abAppend(ab, "~", 1);
+    } else {
+      int len = E.row[filerow].size;
+      if (len > E.screencols)
+        len = E.screencols;
+      abAppend(ab, E.row[filerow].chars, len);
     }
 
     abAppend(ab, "\x1b[K", 3); // clear line currenlty operating on
@@ -313,6 +370,9 @@ void editorProcessKeypress() {
 void initEditor() {
   E.cx = 0;
   E.cy = 0;
+  E.rowoff = 0;
+  E.numrows = 0;
+  E.row = NULL;
   if (getWindowSize(&E.screenrows, &E.screencols) == -1)
     die("getWindowSiza");
 }
@@ -320,6 +380,9 @@ void initEditor() {
 int main(int argc, char **argv) {
   enableRawMode();
   initEditor();
+  if (argc >= 2) {
+    editorOpen(argv[1]);
+  }
 
   while (1) {
     editorRefreshScreen();
