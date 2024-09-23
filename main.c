0a7b807c49cf1ae348b88419952f8d807467e0ef
crative <hannig.sebi@gmail.com>
2024-09-23 15:35:36 +0200

    quittimes

--- main.c
+++ main.c
@@ -18,6 +18,7 @@
 /* defines */
 #define MARKA_VERSION "0.1"
 #define MARKA_TAB_STOP 2
+#define MARKA_QUIT_TIMES 3
 #define CTRL_KEY(k) ((k) & 0x1f)
 
 enum editorKey {
@@ -50,6 +51,7 @@ struct editorConfig {
   int screencols;
   int numrows;
   erow *row;
+  int dirty;
   char *filename;
   char statusmsg[80];
   time_t statusmsg_time;
@@ -261,6 +263,7 @@ void editorAppendRow(char *s, size_t len) {
   editorUpdateRow(&E.row[at]);
 
   E.numrows++;
+  E.dirty++;
 }
 
 void editorRowInsertChar(erow *row, int at, int c) {
@@ -271,6 +274,7 @@ void editorRowInsertChar(erow *row, int at, int c) {
   row->size++;
   row->chars[at] = c;
   editorUpdateRow(row);
+  E.dirty++;
 }
 
 /* editor operations */
@@ -321,6 +325,7 @@ void editorOpen(char *filename) {
 
   free(line);
   fclose(fp);
+  E.dirty = 0;
 }
 
 void editorSave() {
@@ -335,6 +340,7 @@ void editorSave() {
       if (write(fd, buf, len) == len) {
         close(fd);
         free(buf);
+        E.dirty = 0;
         editorSetStatusMessage("%d bytes written to disk", len);
         return;
       }
@@ -425,8 +431,9 @@ void editorDrawRows(struct abuf *ab) {
 void editorDrawStatusBar(struct abuf *ab) {
   abAppend(ab, "\x1b[7m", 4);
   char status[80], rstatus[80];
-  int len = snprintf(status, sizeof(status), "%.20s - %d lines",
-                     E.filename ? E.filename : "[No Name]", E.numrows);
+  int len = snprintf(status, sizeof(status), "%.20s%s - %d lines",
+                     E.filename ? E.filename : "[No Name]", E.dirty ? "*" : "",
+                     E.numrows);
   int rlen = snprintf(rstatus, sizeof(rstatus), "%d/%d", E.cy + 1, E.numrows);
   if (len > E.screencols)
     len = E.screencols;
@@ -523,6 +530,7 @@ void editorMoveCursor(int key) {
 }
 
 void editorProcessKeypress() {
+  static int quit_times = MARKA_QUIT_TIMES;
   int c = editorReadKey();
 
   switch (c) {
@@ -530,6 +538,12 @@ void editorProcessKeypress() {
     // TODO
     break;
   case CTRL_KEY('q'): // case CTRL+Q
+    if (E.dirty && quit_times > 0) {
+      editorSetStatusMessage("File unsaved. Ctrl-Q %d more times to quit.",
+                             quit_times);
+      quit_times--;
+      return;
+    }
     write(STDOUT_FILENO, "\x1b[2J", 4);
     write(STDOUT_FILENO, "\x1b[H", 3);
     exit(0);
@@ -584,6 +598,8 @@ void editorProcessKeypress() {
     editorInsertChar(c);
     break;
   }
+
+  quit_times = MARKA_QUIT_TIMES;
 }
 
 /* init */
@@ -595,6 +611,7 @@ void initEditor() {
   E.coloff = 0;
   E.numrows = 0;
   E.row = NULL;
+  E.dirty = 0;
   E.filename = NULL;
   E.statusmsg[0] = '\0';
   E.statusmsg_time = 0;
